b9da16f964fbe32ec04dc4fdecb49d0e2ba890ed
NastiaDnk <dankoanastasia456@gmail.com>
2022-05-27 03:43:23 +0300

    Full Pacman movements

--- Pacman/Assets/Scripts/Pacman.cs
+++ Pacman/Assets/Scripts/Pacman.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Movements))]
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Movements movement { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        movement = GetComponent<Movements>();
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            movement.SetDirection(Vector2.up);
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            movement.SetDirection(Vector2.left);
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            movement.SetDirection(Vector2.down);
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            movement.SetDirection(Vector2.right);
+
+        float angle = Mathf.Atan2(movement.direction.y, movement.direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+    }
+    
 }
